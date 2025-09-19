c0d48f7b8ed343db977118f7ccb1b5e864a3ac59
Panyaphat1743 <68011743@kmitl.ac.th>
2025-09-19 16:24:36 +0700

    Add files via upload

--- Project.c
+++ Project.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+void Buy_data(){
 
-
-void data(){
+	char Incart[100];
+	FILE*file = fopen("data.csv","a");
+	int choice;
+	int Quantity;
+	char next;
+	
 	printf("data1 price\n");
 	printf("data2 price\n");
 	printf("data3 price\n");	
@@ -18,37 +23,64 @@ void data(){
 	printf("data13 price\n");
 	printf("data14 price\n");
 	printf("data15 price\n");
+	printf("Want something?: ");
+	scanf("%s",&Incart);
+	printf("How many? :");
+	scanf("%d",&Quantity);
+	printf("You want more?\n[1]Yes\n[2]No\n");
+	scanf("%d",&choice);
+
+	while(choice == 1){
+		printf("And you want:");
+		scanf("%s",&Incart);
+		printf("How many? :");
+		scanf("%d",&Quantity);
+		printf("You want more?\n[1]Yes\n[2]No\n");
+		scanf("%d",&choice);
+	}
 }
 //void update_data()
 //void read_csv()
 
 int main(){
-	char Incart[100];
-	FILE*file = fopen("data.csv","w");
-	int next;
-	int Quantity;
 
-
+	int choice;
 	printf("--------------------------------------------------\n");
 	printf("\t  Welcome to Best Item shop XD\n");
-	printf("Let Buy (Press 1)\n");
-	printf("Exit (Press 0)\n");
+	printf("choose your choice\n");
+	printf("(1)Let Buy\n");
+	printf("(2)Update\n");
+	printf("(3)Delete\n");
+	printf("(4)Search\n");
+	printf("(5)Exit\n");
 	printf("--------------------------------------------------\n");
 
-	printf("1 or 0?: ");
-	scanf("%d",&next);
+	printf("Choose: ");
+	scanf("%d",&choice);
 
-	while(next == 1){
-		data();
-		printf("Want something?: ");
-		scanf("%s",&Incart);
-		printf("How many? ");
-		scanf("%d",&Quantity);
-		fprintf(file,"%s,%d",Incart,Quantity);
-		printf("Want more?(choose 1/0): ");
-		scanf("%d",&next);
-		//press ... to update_data()
-		//press ... to read_csv()
+	switch(choice){
+		case 1: //buy
+		Buy_data();
+		break;
+
+		case 2: //Update
+		printf("Update");
+		break;
+
+		case 3: //Delete
+		printf("delete");
+		break;
+
+		case 4: //Search
+		
+		break;
+
+		case 5: //Exit
+		printf("Thank for Buying");
+		break;
+
+		default:
+		printf("Error!!!! What do you select?");
+		break;
 	}
-
 }
